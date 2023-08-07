faf976a0d94eff85e65736e41773dd0f2c812e45
Daniel Morandini <danielmorandini@me.com>
2023-08-07 14:45:32 +0200

    Revert check

--- lib/subtitle/cue/builder.ex
+++ lib/subtitle/cue/builder.ex
@@ -78,9 +78,7 @@ defmodule Subtitle.Cue.Builder do
   end
 
   @spec merge_cues([Cue.t()], [Cue.merge_option()]) :: [Cue.t()]
-  defp merge_cues([], _opts), do: []
-
-  defp merge_cues(cues, opts) do
+  defp merge_cues(cues, opts) when cues != [] do
     cues
     |> tl()
     |> Enum.reduce([hd(cues)], fn next, [cur | done] ->
